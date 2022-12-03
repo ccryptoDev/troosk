0465da71a8cd7c54c6419d83561d02ce9e8ce20c
ccryptoDev <92884804+ccryptoDev@users.noreply.github.com>
2022-12-03 04:32:46 -0400

    dashboard service

--- admin-api/src/modules/dashboard/dashboard.service.ts
+++ admin-api/src/modules/dashboard/dashboard.service.ts
@@ -1,9 +1,13 @@
-import { Injectable, InternalServerErrorException } from '@nestjs/common';
-
+import {
+  HttpStatus,
+  Injectable,
+  InternalServerErrorException,
+} from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
+import { IsNull, Not } from 'typeorm';
+
 import { LoanRepository } from '../../repository/loan.repository';
 import { Flags, StatusFlags } from '../../entities/loan.entity';
-import { getManager } from 'typeorm';
 
 @Injectable()
 export class DashboardService {
@@ -13,47 +17,44 @@ export class DashboardService {
   ) {}
 
   async get() {
-    const entityManager = getManager();
     const data: any = {};
     try {
-      const rawData = await entityManager.query(
-        // `select count(*) as count from tblloan t join tbluser t2 on t2.id = t.user_id  where t.delete_flag = 'N' and t.active_flag = 'Y' and t.status_flag = 'waiting'`,
-        // `select COUNT(*)  from tblloan t where status_flag  = 'waiting'`
-        `select COUNT(*)  from tblloan t where delete_flag = 'N' and active_flag = 'Y' and status_flag = 'pending'`,
-      );
-      data.waiting_application = rawData[0]['count'];
-      console.log('Waiting Application', data.waiting_application);
-
-      const rawData1 = await entityManager.query(
-        // `select count(*) as count from tblloan t join tbluser t2 on t2.id = t.user_id  where t.delete_flag = 'N' and t.active_flag = 'N' and t.status_flag = 'waiting'`,
-        `select COUNT(*)  from tblloan t where delete_flag = 'N' and active_flag = 'N' and status_flag = 'waiting'`,
-      );
-      data.incomplete_application = rawData1[0]['count'];
-      //console.log(rawData[0]['count'])
-      //data.incomplete_application = await this.loanRepository.count({where:{delete_flag:Flags.N,active_flag:Flags.N}})
+      data.waiting_application = await this.loanRepository.count({
+        where: {
+          delete_flag: Flags.N,
+          status_flag: StatusFlags.pending,
+          customer_id: Not(IsNull()),
+          user_id: Not(IsNull()),
+        },
+      });
+      data.incomplete_application = await this.loanRepository.count({
+        where: {
+          delete_flag: Flags.N,
+          status_flag: StatusFlags.waiting,
+          customer_id: Not(IsNull()),
+          user_id: Not(IsNull()),
+        },
+      });
       data.approved_application = await this.loanRepository.count({
         where: {
           delete_flag: Flags.N,
-          active_flag: Flags.Y,
           status_flag: StatusFlags.approved,
+          customer_id: Not(IsNull()),
+          user_id: Not(IsNull()),
         },
       });
       data.canceled_application = await this.loanRepository.count({
         where: {
           delete_flag: Flags.N,
-          active_flag: Flags.Y,
           status_flag: StatusFlags.cancelled,
+          customer_id: Not(IsNull()),
+          user_id: Not(IsNull()),
         },
       });
-      //data.waiting_application = await this.loanRepository.count({where:{delete_flag:Flags.N,active_flag:Flags.Y,status_flag:StatusFlags.waiting}})
 
-      return { statusCode: 200, data: data };
+      return { statusCode: HttpStatus.OK, data };
     } catch (error) {
-      return {
-        statusCode: 500,
-        message: [new InternalServerErrorException(error)['response']['name']],
-        error: 'Bad Request',
-      };
+      throw new InternalServerErrorException(error);
     }
   }
 }
