7ea21568f7f4b7d7e67074624e38ed16ba15646d
ccryptoDev <92884804+ccryptoDev@users.noreply.github.com>
2022-12-07 05:27:10 -0400

    loan contract service

--- borrower-api/src/modules/loan-contract-page/loan-contract.service.ts
+++ borrower-api/src/modules/loan-contract-page/loan-contract.service.ts
@@ -1,6 +1,6 @@
 import {
   BadRequestException,
-  HttpStatus,
+  HttpService,
   Injectable,
   InternalServerErrorException,
   NotFoundException,
@@ -15,13 +15,22 @@ import { CommonService } from '../../common/helper-service';
 import { getLoanAgreement } from './contract/loan-agreement';
 import * as moment from 'moment';
 import { LastScreenModel } from '../../common/last-screen.model';
-import { UserBankAccountRepository } from '../../repository/user-bank-account.repository';
+import {
+  UserBankAccountRepository,
+} from '../../repository/user-bank-account.repository';
 import {
   Flags,
   UserBankAccountsEntity,
 } from '../../entities/user-bank-accounts.entity';
-import { PaymentScheduleService } from '../payment-schedule/payment-schedule.service';
+import {
+  PaymentScheduleService,
+} from '../payment-schedule/payment-schedule.service';
 import { FinicityClient } from '../finicity/finicity.client';
+import { LogService } from '../../common/log.service';
+import { RepayClient } from '../../common/repay-client';
+import {
+  UpdateuserloanService,
+} from '../updateuserloan/updateuserloan.service';
 
 interface PaymentSchedule {
   totalPayment: number;
@@ -42,6 +51,9 @@ export class LoanContractService {
     private readonly loanRepository: LoanRepository,
     private readonly finicityClient: FinicityClient,
     private readonly paymentScheduleService: PaymentScheduleService,
+    private readonly logService: LogService,
+    private readonly userLoanService: UpdateuserloanService,
+    private readonly httpService: HttpService
   ) {}
 
   async getLoanContract(loanId: string) {
@@ -55,7 +67,7 @@ export class LoanContractService {
       const {
         firstName,
         lastName,
-        streetAddress,
+        addressLine1,
         city,
         state,
         finicity_id,
@@ -75,8 +87,8 @@ export class LoanContractService {
       const loanContractData = {
         customerInfo: {
           name: `${firstName} ${lastName}`,
-          address: `${streetAddress}, ${city}, ${state} ${zipCode}`,
-          mobilePhone,
+          address: `${addressLine1}, ${city}, ${state} ${zipCode}`,
+          mobilePhone: this.addDashes(mobilePhone),
         },
         bankDetails: await this.getBankDetails(loan, finicity_id),
         loanDetails: {
@@ -90,9 +102,14 @@ export class LoanContractService {
           numberOfPayments: loan.actualLoanTerm,
           amountOfEachPayment: loan.monthlyPayment,
           paymentDueDate: {
-            dayOfMonth: loan.createdAt.getDate(),
+            dayOfMonth: moment(loan.createdAt).format('Do'),
             payFrequency: LoanPaymentFrequency.monthly,
           },
+          paymentDueDateText: `Monthly, on the ${moment(loan.createdAt).format(
+            'Do',
+          )} day of each month, through ${moment(loan.createdAt)
+            .add(loan.actualLoanTerm, 'month')
+            .format('MMMM YYYY')}`,
         },
         amortizationSchedule: paymentScheduleData.paymentSchedule,
       };
@@ -119,19 +136,40 @@ export class LoanContractService {
         );
         bankDetails = {
           bankName: bankAccount?.account_name,
+          accountType: bankAccount?.ach_account_type,
           accountNumber: bankCredentials.realAccountNumber,
           routingNumber: bankCredentials.routingNumber,
         };
       }
-    } catch (e) {}
+    } catch (e) {
+      this.logService.errorLogs(loan.id, e.message, JSON.stringify(e));
+    }
 
     return bankDetails;
   }
 
   async signLoanContract(loanId: string) {
-    // TODO 1. run hard pull, set up funding, create contract with signature
-    await this.uploadLoanContractInPdf(loanId);
-    return Responses.success('Loan contract signed');
+    // TODO 1. run hard pull
+    const loan = await this.loanRepository.findOne({where: {id: loanId}});
+
+    if (loan === undefined || loan.funding === Flags.Y) {
+      throw new BadRequestException('Loan already funded or not exist');
+    }
+
+    const customer = await this.customerRepository.findOne({where: {id: loan.customer_id}});
+    loan.signature = customer.fullName;
+    loan.lastScreen = LastScreenModel.Sign;
+    this.uploadLoanContractInPdf(loanId);
+    const disbursementResult = await this.disbursement(loanId);
+
+    if (disbursementResult) {
+      loan.funding = Flags.Y;
+      loan.save();
+      return Responses.success('Loan contract signed');
+    } else {
+      loan.save();
+      return Responses.validationError('There is error during funding. Please contact our support')
+    }
   }
 
   async uploadLoanContractInPdf(loanId: string) {
@@ -141,11 +179,8 @@ export class LoanContractService {
       customerInfo.loanDetails.date = moment(
         customerInfo.loanDetails.date,
       ).format('MM/DD/YYYY');
-      const endDate = moment(customerInfo.loanDetails.date)
-        .add(customerInfo.loanDetails.numberOfPayments, 'M')
-        .format('MM/DD/YYYY');
 
-      const htmlData = getLoanAgreement(customerInfo, loanId, endDate);
+      const htmlData = getLoanAgreement(customerInfo, loanId);
       const fileName = `loan-contract.pdf`;
       const uploadedContract = await commonService.convertHTMLToPDF(
         loanId,
@@ -185,11 +220,8 @@ export class LoanContractService {
       customerInfo.loanDetails.date = moment(
         customerInfo.loanDetails.date,
       ).format('MM/DD/YYYY');
-      const endDate = moment(customerInfo.loanDetails.date)
-        .add(customerInfo.loanDetails.numberOfPayments, 'M')
-        .format('MM/DD/YYYY');
 
-      const agreementData = getLoanAgreement(customerInfo, loanId, endDate);
+      const agreementData = getLoanAgreement(customerInfo, loanId);
 
       return Responses.success('Success', {
         agreementData,
@@ -204,5 +236,40 @@ export class LoanContractService {
       if (obj[prop] === Object(obj[prop])) this.mapDeep(obj[prop]);
       else if (obj[prop] === undefined) obj[prop] = '-';
     }
+  }
+
+  private addDashes(phoneNumber: string) {
+    const num = phoneNumber.replace(/\D[^\.]/g, '');
+    return num.slice(0, 3) + '-' + num.slice(3, 6) + '-' + num.slice(6);
+  }
+
+  private async disbursement(loanId) {
+    await this.logService.addLogs(loanId, 'Start disbursement');
+
+    const {
+      disbursement_account_id,
+      actualLoanAmount,
+      customer_id,
+    } = await this.loanRepository.findOne({ where: { id: loanId } });
+
+    let bankAccountsEntity = await this.userBankAccountsRepository.findOne({
+      where: { loan_id: loanId, account_id: disbursement_account_id },
+    });
+
+    const repay = new RepayClient(this.httpService);
+
+    if (!bankAccountsEntity?.repay_token) {
+      bankAccountsEntity = await this.userLoanService.activateACHAccount(loanId, bankAccountsEntity);
+    }
+
+    const disbursement = await repay.ACHInstantDisbursement({
+      ach_token: bankAccountsEntity.repay_token,
+      amount: String(actualLoanAmount),
+      customer_id,
+    });
+
+    await this.logService.addLogs(loanId, JSON.stringify(disbursement?.data));
+
+    return repay.isSuccess(disbursement);
   }
 }
